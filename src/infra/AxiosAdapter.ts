513a5691d88113318489238762cb74c048696182
gabriel761 <jg.7651@gmail.com>
2025-01-16 17:21:27 -0300

    corrigindo problema de autenticação no post, patch, put e delete

--- src/infra/AxiosAdapter.ts
+++ src/infra/AxiosAdapter.ts
@@ -2,28 +2,49 @@ import HttpClient from "@/types/infra-types/HttpClient";
 import axios from "./AxiosConfig"
 
 export default class AxiosAdapter implements HttpClient{
-    async get(url: string, token?:string): Promise<any> {
+    async get(url: string, token?: string): Promise<any> {
         const response = await axios.get(url, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
-        })
-        return response.data
+        });
+        return response.data;
     }
-    async post(url: string, body: any, token: string): Promise<any> {
-        const response = await axios.post(url, body)
-        return response.data
+
+    async post(url: string, body: any, token?: string): Promise<any> {
+        const response = await axios.post(url, body, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
     }
-    async put(url: string, body: any, token: string): Promise<any> {
-        const response = await axios.put(url, body)
-        return response.data
+
+    async put(url: string, body: any, token?: string): Promise<any> {
+        const response = await axios.put(url, body, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
     }
-    async patch(url: string, body: any, token: string): Promise<any> {
-        const response = await axios.patch(url, body)
-        return response.data
+
+    async patch(url: string, body: any, token?: string): Promise<any> {
+        const response = await axios.patch(url, body, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
     }
-    async delete(url: string, body: any, token: string): Promise<any> {
-        const response = await axios.delete(url, {data: body})
-        return response.data
+
+    async delete(url: string, body: any, token?: string): Promise<any> {
+        const response = await axios.delete(url, {
+            data: body,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
     }
 }
